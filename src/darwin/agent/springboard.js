03ed1b6153e2c48d325f15ff254c71db304b5c4c
Ole André Vadla Ravnås <oleavr@gmail.com>
2023-01-24 12:19:47 +0100

    [WIP] Wire up some metadata

--- src/darwin/agent/springboard.js
+++ src/darwin/agent/springboard.js
@@ -1,5 +1,7 @@
 const {
+  LSApplicationProxy,
   NSAutoreleasePool,
+  NSNumber,
 } = ObjC.classes;
 
 const NO = 0;
@@ -11,42 +13,88 @@ rpc.exports = {
   enumerateApplications(identifiers, scope) {
     return performWithAutoreleasePool(() => {
       const identifiers = sbs.copyApplicationDisplayIdentifiers(NO, NO);
-      return mapNSArray(identifiers, identifier => {
-        const name = sbs.copyLocalizedApplicationNameForDisplayIdentifier(identifier);
+      const result = [];
+      for (const identifier of parseNSArray(identifiers)) {
+        try {
+          const name = sbs.copyLocalizedApplicationNameForDisplayIdentifier(identifier);
 
-        let pid;
-        if (sbs.processIDForDisplayIdentifier(identifier, pidPtr))
-          pid = pidPtr.readU32();
-        else
-          pid = 0;
+          let pid;
+          if (sbs.processIDForDisplayIdentifier(identifier, pidPtr))
+            pid = pidPtr.readU32();
+          else
+            pid = 0;
 
-        const parameters = (scope !== 'minimal') ? fetchAppParameters(identifier, scope) : null;
+          const parameters = (scope !== 'minimal') ? fetchAppParameters(identifier, pid, scope) : null;
 
-        return [identifier.toString(), name.toString(), pid, parameters];
-      });
+          result.push([identifier.toString(), name.toString(), pid, parameters]);
+        } catch (e) {
+          console.log(`Skipping ${identifier.toString()}: ${e.stack}`);
+        }
+      }
+      return result;
     });
   }
 };
 
-function fetchAppParameters(identifier, scope) {
-  const parameters = {};
+function fetchAppParameters(identifier, pid, scope) {
+  const parameters = {
+    ...fetchAppMetadata(identifier),
+  };
 
   if (scope === 'full') {
     const icon = sbs.copyIconImagePNGDataForDisplayIdentifier(identifier);
-    if (icon !== null) {
+    if (icon !== null)
       parameters.$icon = icon.base64EncodedStringWithOptions_(0).toString();
-    }
   }
 
   return parameters;
+}
+
+function fetchAppMetadata(identifier, parameters) {
+  const meta = {};
+
+  const app = LSApplicationProxy.applicationProxyForIdentifier_(identifier);
+
+  const version = app.shortVersionString();
+  if (version !== null)
+    meta.version = version.toString();
+
+  const build = app.bundleVersion();
+  if (build !== null)
+    meta.build = build.toString();
+
+  meta.path = app.bundleURL().path().toString();
+
+  const dataPath = app.dataContainerURL();
+  const containerUrls = app.groupContainerURLs();
+  if (dataPath !== null || containerUrls?.count() > 0) {
+    const containers = {};
+
+    if (dataPath !== null)
+      containers.data = dataPath.path().toString();
+
+    if (containerUrls !== null) {
+      for (const [key, value] of Object.entries(parseNSDictionary(containerUrls)))
+        containers[key] = value.path().toString();
+    }
+
+    meta.containers = containers;
+  }
+
+  const getTaskAllow = app.entitlementValueForKey_ofClass_('get-task-allow', NSNumber);
+  if (getTaskAllow?.boolValue())
+    meta.debuggable = true;
+
+  return meta;
 }
 
 function importSpringBoardServices() {
   return [
     ['copyApplicationDisplayIdentifiers', 'pointer', ['bool', 'bool']],
     ['copyLocalizedApplicationNameForDisplayIdentifier', 'pointer', ['pointer']],
-    ['copyIconImagePNGDataForDisplayIdentifier', 'pointer', ['pointer']],
     ['processIDForDisplayIdentifier', 'bool', ['pointer', 'pointer']],
+    ['copyInfoForApplicationWithProcessID', 'pointer', ['uint']],
+    ['copyIconImagePNGDataForDisplayIdentifier', 'pointer', ['pointer']],
   ].reduce((api, [name, retType, argTypes]) => {
     const cname = 'SBS' + name[0].toUpperCase() + name.substring(1);
     const func = new NativeFunction(
@@ -81,10 +129,17 @@ function performWithAutoreleasePool(fn) {
   }
 }
 
-function mapNSArray(arr, fn) {
+function parseNSArray(arr) {
   const result = [];
   const count = arr.count().valueOf();
   for (let i = 0; i !== count; i++)
-    result.push(fn(arr.objectAtIndex_(i)));
+    result.push(arr.objectAtIndex_(i));
+  return result;
+}
+
+function parseNSDictionary(dict) {
+  const result = {};
+  for (const key of parseNSArray(dict.allKeys()))
+    result[key.toString()] = dict.valueForKey_(key);
   return result;
 }
